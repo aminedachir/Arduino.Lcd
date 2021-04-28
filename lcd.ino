f7825144973c8d655b77fef3f7d679a8cdcf4fb1
aminedachir <dachiramine110@gmail.com>
2021-04-28 01:07:12 +0100

    use delay function

--- lcd.ino
+++ lcd.ino
@@ -1,11 +1,18 @@
-#include <LiquidCrystal.h>
-LiquidCrystal lcd( 8,  9,  4,  5,  6, 7);
+
+#include "LiquidCrystal.h"
+LiquidCrystal lcd( 8,  9,  4,  5,  6,  7);
 void setup() {
- lcd.begin(10, 2);
- lcd.setCursor(0,0);
- lcd.print("YOUSSEF   ");
- lcd.setCursor(0,1); 
- lcd.print(" AMINE DACHIR ");
+ lcd.begin(16, 2);
+ lcd.setCursor(4,0);
+ lcd.print("MOHAMMED");
 }
 void loop() {
+  for (int i = 0;i<3;i++){
+    delay(1000);
+    lcd.setCursor(2,1);
+    if (i = 1){lcd.print ("AMINE");}
+    delay(1500);
+    lcd.setCursor(9,1);
+    if (i = 2){lcd.print ("DACHIR");}
+  } 
 }
