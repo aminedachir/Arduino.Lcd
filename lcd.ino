8bc9ef6f715b8529b79c1926ef397fef23e4b5ee
aminedachir <dachiramine110@gmail.com>
2021-04-28 01:40:02 +0100

    update from lcd screen

--- lcd.ino
+++ lcd.ino
@@ -3,16 +3,19 @@
 LiquidCrystal lcd( 8,  9,  4,  5,  6,  7);
 void setup() {
  lcd.begin(16, 2);
- lcd.setCursor(4,0);
- lcd.print("MOHAMMED");
 }
 void loop() {
   for (int i = 0;i<3;i++){
+    delay(500);
+    lcd.setCursor(4,0);
+    lcd.print("MOHAMMED");
     delay(1000);
     lcd.setCursor(2,1);
     if (i = 1){lcd.print ("AMINE");}
-    delay(1500);
+    delay(1000);
     lcd.setCursor(9,1);
     if (i = 2){lcd.print ("DACHIR");}
+    delay(1000);
+    lcd.clear();
   } 
 }
