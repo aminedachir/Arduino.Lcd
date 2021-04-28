a4b581a4db496130ef17699c94a578f88e07be41
AmineDachir <68955063+aminedachir@users.noreply.github.com>
2021-04-28 17:16:12 +0100

    Add files via upload

--- lcd1.ino
+++ lcd1.ino
@@ -1,11 +1,27 @@
 #include "LiquidCrystal.h"
-LiquidCrystal lcd(8, 9, 4, 5, 6,7);
+LiquidCrystal lcd( 8, 9, 4, 5, 6,7);
 void setup() {
+  int a = 0;
   lcd.begin(2,2);
+  for (int i=0;i<=8;i++){
+    lcd.setCursor(a,1);
+    lcd.write(Serial.read());
+    a = a + 1;
+    delay(400);
+    lcd.setCursor(a,1);
+    lcd.write(Serial.read());
+    a = a + 1;
+    delay(400);
+  }
+  lcd.clear();
+  delay(400);
 }
 void loop() {
+  lcd.setCursor(3,0);
+  lcd.print("Succefully");
+  lcd.setCursor(3,1);
+  lcd.print("Downloading");
   delay(1000);
-  lcd.setCursor(1,0);
-  lcd.print("<!--MOHAMMED AMINNE DACHIR-->");
-  lcd.scrollDisplayLeft();
+  lcd.clear()
+  delay(400);
 }
